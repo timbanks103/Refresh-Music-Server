1da0b174c2fc52cd3bebca36d2a42430b2dc6e9b
Tim Banks <timbanks103@gmail.com>
2025-04-04 16:53:14 +0100

    Stable base version Incorporated playlist management and Fixed Issue 1

--- transcoding.py
+++ transcoding.py
@@ -33,11 +33,10 @@ logger = logging.getLogger(__name__)
 
 sourceDir = '/Volumes/Media/Shared Music/Music Library' # Source of all music
 mp3Dir = '/Volumes/Media/Shared Music/MusicMP3/Music'
-fnameExclusions=[".DS_Store",
-                "Folder.jpg",  # Preserves existing artwork not present in Mac OS  Music.app
-                "folder.jpg" ]
+fnameExclusions=[".DS_Store"]
 dnameExclusions=["Automatically Add to Music.localized",
                 "Music Library.musiclibrary"]
+fkinds =[".m4a",".mp3",".M4A",".MP3",".jpg",".m3u",".m3u8",".pdf"]
 
 # Damage limitation
 #examConstraint = 10000000
@@ -52,32 +51,38 @@ for (r,ds,ls) in os.walk(sourceDir,topdown=True):
   #  if len(ds)==0:
         for l in ls:
             if l not in fnameExclusions and r[dlen+1:].split("/")[0] not in dnameExclusions and not l.startswith("."): # and len(sourceList) <= examConstraint:
-                sourceDates[r[dlen+1:]+"/"+l]=os.path.getmtime(r+"/"+l)
-                sourceList.append(r[dlen+1:]+"/"+l)
+                if r!=sourceDir: lf = "/"+l
+                else: lf ="/./"+l # File need a directory separator to make subsequent processing more straightforward
+                sourceDates[r[dlen+1:]+lf]=os.path.getmtime(r+lf)
+                if os.path.splitext(lf)[1] in fkinds: sourceList.append(r[dlen+1:]+lf)
                 
 sourceList.sort()
 
 
 mp3List=[]
 mp3Dates={}
+emptydirs=[]
 dlen=len(mp3Dir)
 for (r,ds,ls) in os.walk(mp3Dir,topdown=True):
-    if ds==[]:
+        if ls==[]: emptydirs.append(r)
         for l in ls:
             if l not in fnameExclusions: # and len(mp3List) <= examConstraint:
-                mp3Dates[r[dlen+1:]+"/"+l]=os.path.getmtime(r+"/"+l)
-                mp3List.append(r[dlen+1:]+"/"+l)# l.replace(".mp3", ""))
+                if r!=mp3Dir: lf = "/"+l
+                else: lf ="/./"+l
+                mp3Dates[r[dlen+1:]+lf]=os.path.getmtime(r+lf)
+                mp3List.append(r[dlen+1:]+lf)
 mp3List.sort()
 
+
 # Report the discoveries
-additions=[f for f in sourceList if f.replace(".m4a", ".mp3")
-    .replace(".M4A", ".mp3") not in mp3List]
-additions = additions+ [f for f in sourceList if f.find("Folder.jpg")>-1 and f not in mp3List]
+
+
+additions=[f for f in sourceList if f.replace(".m4a", ".mp3").replace(".M4A", ".mp3") not in mp3List]
 removals=[f for f in mp3List if f not in sourceList and f.replace(".mp3", ".m4a") not in sourceList and f.replace(".mp3", ".M4A") not in sourceList ]
 updates=[f for f in sourceList if (f not in additions and (sourceDates[f]>mp3Dates[f.replace(".m4a", ".mp3").replace(".M4A", ".mp3")] or sourceDates[f]>mp3Dates[f.replace(".m4a", ".mp3").replace(".M4A", ".mp3")])) ]
 
-
-
+logger.info(pformat(additions[0:3]))
+if len(additions)>3: print ("of "+str(len(additions)))
 logger.info("Additions to "+sourceDir+" not in "+mp3Dir+":")
 logger.info(pformat(additions[0:3]))
 if len(additions)>3: print ("of "+str(len(additions)))
@@ -96,8 +101,19 @@ if len(removals)>0:
      if removed <= removalConstraint:
         os.remove(mp3Dir+"/"+f)
         removed=removed+1
-        logger.info("Removed extraneous file: "+f)
+        opath=mp3Dir+"/"+f
+        logger.info(f"Removed extraneous file {opath} ({removed} of {len(removals)}). ")
 
+
+
+emptydirs=[]
+dlen=len(mp3Dir)
+for (r,ds,ls) in os.walk(mp3Dir,topdown=True):
+        if ls==[] and ds==[]: emptydirs.append(r)
+
+logger.info("Empty Directories: ")
+logger.info(pformat(emptydirs[0:30]))
+if len(emptydirs)>3: logger.info("of "+str(len(emptydirs)))
 
 # Convert video format
 
@@ -106,8 +122,9 @@ import ffmpeg, shutil
 if len(updates+additions)>0:
     transcoded=0
     copied=0
+    skipped=0
     for f in updates+additions:
-        if f.find(".m4a") > -1 and transcoded<=transcodeConstraint:
+        if (f.find(".m4a") > -1 or f.find(".M4A") > -1) and transcoded<=transcodeConstraint:
             input_path = sourceDir+"/"+f
             output_path = mp3Dir+"/"+f.replace(".m4a", ".mp3").replace(".M4A", ".mp3")
             output_dir = "/".join(output_path.split("/")[0:-1])
@@ -117,23 +134,55 @@ if len(updates+additions)>0:
                 if f in updates: msgInsert = "updated"
                 else: msgInsert = "missing"
                 transcoded=transcoded+1
-                logger.info(f"Successfully converted {msgInsert} {input_path} to {output_path}. ({transcoded-copied} of {len(updates+additions)}). ")
+                logger.info(f"Successfully converted {msgInsert} {input_path} to {output_path}. ({transcoded+copied} of {len(updates+additions)}). ")
             except ffmpeg.Error as e:
                 logger.debug(f"An error occurred attempting to transcode {f}: {e}")
                 raise Exception(f"An error occurred: {e} attempting to transcode {f}")
-        elif f.lower().find(".mp3") or f.lower().find(".jpg") > -1 or f.lower().find(".pdf") and copied<=copyConstraint:
+        elif f.lower().find(".mp3") >-1 or f.lower().find(".jpg") > -1 or f.lower().find(".pdf") >-1 and copied<=copyConstraint:
                 input_path = sourceDir+"/"+f
                 output_path = mp3Dir+"/"+f
                 output_dir = "/".join(output_path.split("/")[0:-1])
                 os.makedirs(output_dir, exist_ok=True)
                 rc=shutil.copy2(input_path, output_dir)
-
+                if f in updates: msgInsert = "updated"
+                else: msgInsert = "missing"
                 if rc==output_path:
                     copied = copied+1
-                    logger.info(f"Copied {f} to {output_path}. ({copied} of {len(updates+additions)-transcoded}). ")
+                    logger.info(f"Copied {msgInsert} {f} to {output_path}. ({transcoded+copied+skipped} of {len(updates+additions)}). ")
                     
                 else:
                     raise Exception(f"Failed copy of {input_path} to {output_path}. cp returned {rc}")
+        elif f.lower().find(".m3u") >-1 and copied<=copyConstraint:
+                input_path = sourceDir+"/"+f
+                output_path = mp3Dir+"/"+f
+                output_dir = "/".join(output_path.split("/")[0:-1])
+                os.makedirs(output_dir, exist_ok=True)
+                rc=shutil.copy2(input_path, output_dir)
+                # Edit the file here to replace m4a with mp3 and convert absolute paths to relative.
+                # Read in the file
+                with open(output_path, 'r') as file:
+                    filedata = file.read()
+
+                # Replace the target strings
+                filedata = filedata.replace(sourceDir, '.').replace('.m4a','.mp3')
+                # Write the file out again
+                with open(output_path, 'w') as file:
+                    file.write(filedata)
+		
+                if f in updates: msgInsert = "updated"
+                else: msgInsert = "missing"
+                if rc==output_path:
+                    copied = copied+1
+                    logger.info(f"Copied/translated {msgInsert} {f} to {output_path}. ({transcoded+copied+skipped} of {len(updates+additions)}). ")
+                    
+                else:
+                    raise Exception(f"Failed copy of {input_path} to {output_path}. cp returned {rc}")
+        
+        else:
+            skipped=skipped+1
+            logger.info(f"Skipped {f}. Wrong file type. ({transcoded+copied+skipped} of {len(updates+additions)}). ")
+            
+        
                     
 else: logger.info("No transcoding or copying needed. ")
     
