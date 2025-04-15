6b29e3c692b71adb5c139f1232163c34c62eb6bd
Tim Banks <timbanks103@gmail.com>
2025-04-15 11:33:35 +0100

    Corrected addition of new components

---
+++ playlistAssembly.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""  main.py
+
+   Assembles playlist media files into consolidated folders so they can be
+   played from a usb Flash drive 
+
+
+
+"""
---
+++ usbCleanup.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# ._ files (which are numerous) are not useful to a media player but are visible in the UI during media selection. 
+# This script cleans the Music folders of these files.
+
+# This script may run as root when called as a preflight script by Carbon Copy Cloner
+
+cd /Users/timbanks/MyWork/Refresh\ Music\ Server
+
+
+# This dot_clean command gets rid of the extraneous files.
+dot_clean /Volumes/Portable/MusicMP3 
+
+exit
